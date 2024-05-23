3c076d340a4e544deb70b00adf43ead57c388f5b
Naimul9 <naimulakib100@gmail.com>
2024-05-23 21:37:43 +0600

    env file updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const corsOption ={
 app.use(cors(corsOption))
 app.use(express.json())
 
-const uri = "mongodb+srv://naimulakib100:uBOcOty0njNskorh@cluster0.ahphq0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ahphq0t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
